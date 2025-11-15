84278d6e9d8def02356e3ff8d434106ba39da518
matin <matinehelahi@gmail.com>
2025-11-15 23:58:02 +0330

    add 3 practice

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//1
+
+// Console.Write("What your name?");
+// string name = Console.ReadLine();
+
+// Console.WriteLine("Hi "+name+"!");
+
+
+//2
+// Console.Write("Enter a Number ? ");
+// int num1 =int.Parse(Console.ReadLine());
+// Console.Write("Enter a Number? ");
+// int num2=int.Parse(Console.ReadLine());
+// int sum = num1+num2;
+// Console.WriteLine("Sum of " +num1 +" and "+num2+" is " +sum);
+
+
+//3 
+// Console.Write("How old are you? ");
+
+// int age = int.Parse(Console.ReadLine());
+//  if (age >= 18)
+// {
+//     Console.WriteLine("yes");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+
